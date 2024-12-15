029b5e217e2c9966e2e6368ea2070b7aa5123790
21JC <jiayangcheng21@gmail.com>
2024-12-15 01:43:09 -0800

    basic working

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -31,21 +31,21 @@ var config = {
      */
     imgs: {
         "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
+        "心爱的姑娘": "./imgs/rui.png",
         "今天是你的生日": "./imgs/shengri.jpeg",
         "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
+        "第三个生日！": "./imgs/3.png",
         "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
+        "奇怪的样子": "./imgs/qiguai.png",
+        "可爱的样子": "./imgs/keai.png",
+        "生气的样子": "./imgs/shengqi.png",
+        "都是我喜欢的样子": "./imgs/chongai.png",
         "对了，礼物收全了吗？": "./imgs/chuo.gif",
         "今年要吃好好的": "./imgs/chihaodian.jpeg",
         "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        "然后才有力气干活": "./imgs/ganhuo.png",
+        "和想我！": "./imgs/together.png",
+        "生日快乐~~": "./imgs/26.gif"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -61,5 +61,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '爱你的J'
 };
